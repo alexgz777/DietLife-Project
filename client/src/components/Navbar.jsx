0fce588e5020fd893804aaaef2364f84c32985fd
alexgz777 <alexgz777@gmail.com>
2022-07-27 20:22:26 -0300

    commit 2 22/07/22

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Searchbar from "./Searchbar";
 import "../styles/Navbar.css";
+import { Link } from "react-router-dom";
 /* import Dietlife from "../assets/Dietlife.png";
  */
 const Navbar = () => {
@@ -9,18 +10,18 @@ const Navbar = () => {
       <li className="navbar__li">
         {/*         <img src={Dietlife} alt="logo" className="navbar__logo" />
          */}{" "}
-        <a href="/" className="navbar__btn">
-          Start
-        </a>
-        <a href="/home" className="navbar__btn">
-          Home
-        </a>
-        <a href="/create" className="navbar__btn">
-          Create
-        </a>
-        <a href="/about" className="navbar__btn">
-          About Us
-        </a>
+        <Link to={`${process.env.VITE_API_KEY}`} className="navbar__btn">
+          <button>Start</button>
+        </Link>
+        <Link to={`${process.env.VITE_API_KEY}/home`} className="navbar__btn">
+          <button>Home</button>
+        </Link>
+        <Link to={`${process.env.VITE_API_KEY}/create`} className="navbar__btn">
+          <button>Create</button>
+        </Link>
+        <Link to={`${process.env.VITE_API_KEY}/about`} className="navbar__btn">
+          <button> About Us</button>
+        </Link>
         <Searchbar />
       </li>
     </ul>
